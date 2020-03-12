02ff96953f8283b160ab17fc8989a68fd4ee7308
weldersantoos <weldersantoos7@gmail.com>
2020-03-12 20:03:43 -0300

    test

---
+++ exemplo_01/script.js
@@ -0,0 +1,12 @@
+function soma() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    var elemResult = document.getElementById("resultado");
+
+    if (elemResult.textContent === undefined) {
+       elemResult.textContent = "O resultado é " + String(num1 + num2) + ".";
+    }
+    else { // IE
+       elemResult.innerText = "O resultado é " + String(num1 + num2) + ".";
+    }
+}
